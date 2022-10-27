04518f8ba72d3e322b6639e7eb1c2ebbe8eecb8f
PageAds <adampage6@gmail.com>
2022-10-27 22:19:13 +0100

    TDD - GET /pokemon/translated/{pokemonName} integration test for failing fun transactions api client passed

--- src/Pokedex.Services/PokemonTranslatorService.cs
+++ src/Pokedex.Services/PokemonTranslatorService.cs
@@ -1,4 +1,5 @@
-﻿using Pokedex.Common.Models;
+﻿using Microsoft.Extensions.Logging;
+using Pokedex.Common.Models;
 using Pokedex.Data.HttpClients;
 using Pokedex.Data.Models.FunTranslationsApi;
 using Pokedex.Data.Repositories;
@@ -7,13 +8,16 @@ namespace Pokedex.Services
 {
     public class PokemonTranslatorService : IPokemonTranslatorService
     {
+        private readonly ILogger<PokemonTranslatorService> logger;
         private readonly IPokemonRepository pokemonRepository;
         private readonly IFunTranslationsApiClient funTranslationApiClient;
 
         public PokemonTranslatorService(
+            ILogger<PokemonTranslatorService> logger,
             IPokemonRepository pokemonRepository,
             IFunTranslationsApiClient funTranslationApiClient)
         {
+            this.logger = logger;
             this.pokemonRepository = pokemonRepository;
             this.funTranslationApiClient = funTranslationApiClient;
         }
@@ -22,10 +26,17 @@ namespace Pokedex.Services
         {
             var pokemon = await pokemonRepository.Get(pokemonName);
 
-            if (ShouldPerformYodaTranslation(pokemon))
-                pokemon.Description = await funTranslationApiClient.GetTranslation(pokemon.Description, TranslationType.Yoda);
-            else
-                pokemon.Description = await funTranslationApiClient.GetTranslation(pokemon.Description, TranslationType.Shakespeare);
+            try
+            {
+                if (ShouldPerformYodaTranslation(pokemon))
+                    pokemon.Description = await funTranslationApiClient.GetTranslation(pokemon.Description, TranslationType.Yoda);
+                else
+                    pokemon.Description = await funTranslationApiClient.GetTranslation(pokemon.Description, TranslationType.Shakespeare);
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning(ex, "Unable to perform translation of description, using standard description instead");
+            }
 
             return pokemon;
         }
